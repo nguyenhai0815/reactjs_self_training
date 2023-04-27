701abf7680a44a40a9e6f9b37d60a5d1ad142235
nguyenhai1508 <nguyenhaind0815@gmail.com>
2023-04-27 13:52:29 +0700

    state - lifecycle update

--- 05_state-and-lifecycle/src/App.js
+++ 05_state-and-lifecycle/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      count2: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count); // giá trị không thay đổi ngay lập tức
+  };
+  handleIncrement2 = () => {
+    // gọi đến callback
+    this.setState({ count2: this.state.count2 + 1 }, () => {
+      console.log(this.state.count2);
+    });
+  };
+  
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <h1>{this.state.count2}</h1>
+        <button onClick={this.handleIncrement2}>Increment2</button>
+      </div>
+    );
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock />
+      <Example/>
     </div>
   );
 }
