aafb0fbbd11a7d3263191a2c972f97ffab73eda4
nguyenhai1508 <nguyenhaind0815@gmail.com>
2023-04-28 11:29:50 +0700

    09_Form

--- 09_form/README.md
+++ 09_form/README.md
@@ -20,3 +20,41 @@
   - value: Thuộc tính này được sử dụng để đặt giá trị cho field trong form.
 
 ## 1. Controlled Components
+
+#### Với HTML thì các phần tử như `<input>` `<textarea>` `<select>` là các phần tử thường được sử dụng để thay đổi giá trị của form và các thay đổi này sẽ được update dựa trên việc người dùng thay đổi input.
+
+#### Trong React, các trạng thái thay đổi (mutable state) thì sẽ được giữ trong state của component, và chỉ được cập nhật khi sử dụng hàm setState().
+
+#### Có thể kết hợp form và React State để có được "Controlled component".
+
+- React Component sẽ render form cùng các control.
+- Và khi user thay đổi giá trị của input thì chúng ta có thể sử dụng setState() để update cho component state luôn
+
+#### Với việc kết hợp này chúng ta có thể dễ dàng handle được việc các giá trị thay đổi, cũng như khi submit form.
+
+## 2. textarea Tag
+
+#### Với React, thay vì nằm bên trong, thẻ `<textarea>` sẽ sử dụng thuộc tính value để lưu trữ các đoạn văn bản.
+
+#### Khi sử dụng cách này, một form có chứa `<textarea>` sẽ có thể được biểu diễn tương tự như một form sử dụng thẻ input tự đóng `<input />`
+
+## 3. select Tag
+
+Điểm khác biệt giữa HTML thuần và React khi sử dụng thẻ `<select> `như sau:
+
+HTML sử dụng attribute selected để xác định option được select, còn với React thì sử dụng attribute value
+
+## 4,Thẻ input file
+
+#### Bởi vì giá trị của file input chặn quyền ghi (read-only), nên nó là một uncontrolled component trong React.
+
+#### Để lấy giá trị từ một input file trong React, chúng ta cần sử dụng ref để truy cập đến element input đó và lấy giá trị từ thuộc tính files của nó.
+
+## 5. Handling Multiple Inputs
+
+#### Khi cần xử lí nhiều controlled input, có thể thêm thuộc tính name vào từng element và để hàm xử lí (handler function) lựa chọn được chính xác element nào đang tương tác với người dùng thông qua event.target.name
+
+## 6. Controlled Input với giá trị null
+
+- Khi chúng ta set cứng value cho input thì giá trị của input sẽ ko bị thay đổi bởi người dùng
+- Nếu muốn cho phép người dùng thay đổi giá trị thì set value cho nó bằng Null
--- 09_form/src/App.js
+++ 09_form/src/App.js
@@ -1,23 +1,224 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Container, Form, Button, FormControl } from 'react-bootstrap';
+
+class FormExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      textareaValue: "",
+      grapeFruit: "",
+      selectedCity: "",
+      selectedOptions: [],
+      selectedFile: null,
+    };
+    this.fileInputRef = React.createRef();
+    this.cities = ["Hanoi", "Ho Chi Minh City", "Da Nang", "Hai Phong", "Can Tho"];
+  }
+
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleChangeSelect = (event) => {
+    this.setState({ grapeFruit: event.target.value });
+  };
+
+  handleChangeSelectCity = (event) => {
+    this.setState({ selectedCity: event.target.value });
+  };
+
+  handleChangeMultipart = (event) => {
+    const selectedOptions = Array.from(event.target.selectedOptions, (option) => option.value);
+    this.setState({ selectedOptions });
+  }
+
+  handleChangeTextarea = (event) => {
+    this.setState({ textareaValue: event.target.value });
+  };
+
+  handleFileChange = (event) => {
+    this.setState({ selectedFile: event.target.files[0] });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.selectedFile);
+
+    alert(
+      "A name was submitted: " + this.state.name + "\n" +
+      "A textarea was submitted: " + this.state.textareaValue + "\n" +
+      "A grapeFruit was submitted: " + this.state.grapeFruit + "\n" +
+      "Selected city: " + this.state.selectedCity + "\n" +
+      `Selected Options: ${this.state.selectedOptions}`
+    );
+  };
+
+  handleChangeEvent = (event) => {
+    const { name, type } = event.target;
+    const value = type === 'checkbox' ? event.target.checked : event.target.value;
+    if (name === 'selectedOptions') {
+      const options = event.target.selectedOptions;
+      const values = [];
+      for (let i = 0, l = options.length; i < l; i++) {
+        if (options[i].selected) {
+          values.push(options[i].value);
+        }
+      }
+      this.setState({ [name]: values });
+    } else if (name === 'selectedFile') {
+      this.setState({ [name]: event.target.files[0] });
+    } else {
+      this.setState({ [name]: value });
+    }
+  };
+
+  handleSubmitEvent = (event) => {
+    event.preventDefault();
+
+    alert(
+      "A name was submitted: " + this.state.name + "\n" +
+      "A textarea was submitted: " + this.state.textareaValue + "\n" +
+      "A grapeFruit was submitted: " + this.state.grapeFruit + "\n" +
+      "Selected city: " + this.state.selectedCity + "\n" +
+      `Selected Options: ${this.state.selectedOptions}`+ "\n" +
+      `selectedFile: ${this.state.selectedFile.name}`
+    );
+  };
+
+  render() {
+    return (
+      <Container>
+        <Form onSubmit={this.handleSubmit}>
+
+          {/* input tag */}
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" name="name" value={this.state.name} onChange={this.handleChange} />
+          </Form.Group>
+
+          {/* textarea tag */}
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="Enter some text" value={this.state.textareaValue} onChange={this.handleChangeTextarea} />
+          </Form.Group>
+
+          {/* sellect tag */}
+          <Form.Group controlId="exampleForm.SelectCustom">
+            <Form.Label>Select your favorite fruit:</Form.Label>
+            <Form.Control as="select" value={this.state.grapeFruit} onChange={this.handleChangeSelect}>
+              <option>Grapefruit</option>
+              <option value="Lime">Lime</option>
+              <option value="Coconut">Coconut</option>
+              <option value="Mango">Mango</option>
+            </Form.Control>
+          </Form.Group>
+
+          {/* select tag */}
+          <Form.Group controlId="exampleForm.SelectCustom">
+            <Form.Label>Select your city:</Form.Label>
+            <Form.Control as="select" value={this.state.selectedCity} onChange={this.handleChangeSelectCity}>
+              {this.cities.map((city) => (
+                <option key={city}>{city}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+
+          {/* sellect multipart */}
+          <Form.Group controlId="exampleForm.ControlSelect2">
+            <Form.Label>Select multiple options:</Form.Label>
+            <Form.Control as="select" multiple onChange={this.handleChangeMultipart}>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+              <option value="option4">Option 4</option>
+              <option value="option5">Option 5</option>
+            </Form.Control>
+          </Form.Group>
+
+          {/* File input */}
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Select a file:</Form.Label>
+            <Form.Control type="file" ref={this.fileInputRef} onChange={this.handleFileChange} />
+          </Form.Group>
+          
+          <br/>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+
+        {/* Handling Multiple Inputs */}
+        <Form onSubmit={this.handleSubmitEvent}>
+
+          {/* input tag */}
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" name="name" value={this.state.value} onChange={this.handleChangeEvent} />
+          </Form.Group>
+
+          {/* textarea tag */}
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} name="textareaValue" placeholder="Enter text" value={this.state.textareaValue} onChange={this.handleChangeEvent} />
+          </Form.Group>
+
+          {/* sellect tag */}
+          <Form.Group controlId="exampleForm.SelectCustom">
+            <Form.Label>Select your favorite fruit:</Form.Label>
+            <Form.Control as="select" name="grapeFruit" value={this.state.grapeFruit} onChange={this.handleChangeEvent}>
+              <option>Grapefruit</option>
+              <option value="Lime">Lime</option>
+              <option value="Coconut">Coconut</option>
+              <option value="Mango">Mango</option>
+            </Form.Control>
+          </Form.Group>
+
+          {/* select tag */}
+          <Form.Group controlId="exampleForm.SelectCustom">
+            <Form.Label>Select your city:</Form.Label>
+            <Form.Control as="select" name="selectedCity" value={this.state.selectedCity} onChange={this.handleChangeEvent}>
+              {this.cities.map((city) => (
+                <option key={city}>{city}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+
+          {/* sellect multipart */}
+          <Form.Group controlId="exampleForm.ControlSelect2">
+            <Form.Label>Select multiple options:</Form.Label>
+            <Form.Control as="select" name="selectedOptions" multiple onChange={this.handleChangeEvent}>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+              <option value="option4">Option 4</option>
+              <option value="option5">Option 5</option>
+            </Form.Control>
+          </Form.Group>
+
+          {/* File input */}
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Select a file:</Form.Label>
+            <Form.Control type="file" name="selectedFile" ref={this.fileInputRef} onChange={this.handleChangeEvent} />
+          </Form.Group>
+          
+          <br/>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormExample/>
     </div>
   );
 }
