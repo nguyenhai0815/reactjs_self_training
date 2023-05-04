ab3504ae6ca0584766f3453d1d80780b2675402c
nguyenhai1508 <nguyenhaind0815@gmail.com>
2023-05-04 14:42:22 +0700

    13 Context

---
+++ 13_context/src/Button.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+
+class ThemedButton extends React.Component {
+  render() {
+    let props = this.props;
+    let theme = this.context;
+    return (
+      <button
+        {...props}
+        style={{backgroundColor: theme.background}}
+      />
+    );
+  }
+}
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
