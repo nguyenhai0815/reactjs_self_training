a4b5153d3d7fb49f2e3c5c74c10c5e2804b2c9b6
nguyenhai1508 <nguyenhaind0815@gmail.com>
2023-05-10 14:25:32 +0700

    Render-ilist-item

--- project-todo-list/src/components/Item.js
+++ project-todo-list/src/components/Item.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 
-class Item extends Component{
-    render() {
-        return (
-            <tr>
-                <td className="text-center">1</td>
-                <td>Tìm thấy mảnh vỡ máy bay rơi ở Iran làm 66 người chết</td>
-                <td className="text-center"><span className="label label-danger">High</span></td>
-                <td>
-                    <button type="button" className="btn btn-warning btn-sm">Edit</button>
-                    <button type="button" className="btn btn-danger btn-sm">Delete</button>
-                </td>
-            </tr>
-        )
+function Item(props) {
+    const levelInfor = {
+        0: {
+            className: 'label label-info',
+            label: 'Low',
+        },
+        1: {
+            className: 'label label-warning',
+            label: 'Medium',
+        },
+        2: {
+            className: 'label label-danger',
+            label: 'Hight',
+        }
     }
+    
+    const level = props.data.level;
+    const { label, className } = levelInfor[level];
+
+    return (
+        <tr>
+            <td className="text-center">{props.index + 1}</td>
+            <td>{ props.data.name }</td>
+            <td className="text-center"><span className={className}>{ label }</span></td>
+            <td>
+                <button type="button" className="btn btn-warning btn-sm">Edit</button>&nbsp;
+                <button type="button" className="btn btn-danger btn-sm">Delete</button>
+            </td>
+        </tr>
+    )
 }
 
 export default Item;
